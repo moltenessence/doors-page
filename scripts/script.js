395220fbb2053f37a7557d57209e186f52559536
Daniil Feoktistov <whatdoessence@gmail.com>
2022-11-17 19:26:01 +0300

    Rename some variables

--- scripts/script.js
+++ scripts/script.js
@@ -1,72 +1,88 @@
-let pos1 = 0;
-let pos2 = 0;
+const MAIN_SLIDER_TYPE = 1;
+const REVIEWS_SLIDER_TYPE = 2;
 
-const itemWidth1 = () => document.querySelector('#main-slider .slider-container').clientWidth;
-const itemWidth2 = () => document.querySelector('#reviews .slider-container').clientWidth;
+let mainSliderPosition = 0;
+let reviewsSliderPosition = 0;
 
-const track1 = document.querySelector('#main-slider .slider-track'); 
-const track2 = document.querySelector('#reviews .slider-track'); 
+const countMainSliderWidth = () => document.querySelector('#main-slider .slider-container').clientWidth;
+const countReviewsSliderWidth = () => document.querySelector('#reviews .slider-container').clientWidth;
 
-const btnNext1 = document.querySelector('#main-slider .btn-next');
-const btnPrev1 = document.querySelector('#main-slider .btn-prev');
-const btnNext2 = document.querySelector('#reviews .btn-next');
-const btnPrev2 = document.querySelector('#reviews .btn-prev');
+const mainSliderTrack = document.querySelector('#main-slider .slider-track');
+const reviewsSliderTrack = document.querySelector('#reviews .slider-track');
 
-const act = 'active'; 
+const mainSliderNextButton = document.querySelector('#main-slider .btn-next');
+const mainSliderPreviousButton = document.querySelector('#main-slider .btn-prev');
+const reviewsSliderNextButton = document.querySelector('#reviews .btn-next');
+const reviewsSliderPreviousButton = document.querySelector('#reviews .btn-prev');
 
-function btnNext(a) {
-  if (a == 1) pos1 -= itemWidth1(); 
-  else pos2 -= itemWidth2();
-  
-  setPosition(a); 
-  checkButtons(a);
+const activeAttribute = 'active';
+
+function swapSliderTowards(sliderType) {
+
+  switch (sliderType) {
+    case MAIN_SLIDER_TYPE:
+      mainSliderPosition -= countMainSliderWidth();
+      break;
+    case REVIEWS_SLIDER_TYPE:
+      reviewsSliderPosition -= countReviewsSliderWidth();
+      break;
+  }
+
+  setPosition(sliderType);
+  checkButtons(sliderType);
 }
 
-function btnPrev(a) {
-  if (a == 1) pos1 += itemWidth1();
-  else pos2 += itemWidth2();
+function swapSliderBackwards(sliderType) {
+  switch (sliderType) {
+    case MAIN_SLIDER_TYPE:
+      mainSliderPosition += countMainSliderWidth();
+      break;
+    case REVIEWS_SLIDER_TYPE:
+      reviewsSliderPosition += countReviewsSliderWidth();
+      break;
+  }
 
-  setPosition(a);
-  checkButtons(a); 
+  setPosition(sliderType);
+  checkButtons(sliderType);
 }
 
-const setPosition = (a) => { 
-  if (a == 1) track1.style.transform = `translateX(${pos1}px)`;
-  else track2.style.transform = `translateX(${pos2}px)`;
+const setPosition = (a) => {
+  if (a == MAIN_SLIDER_TYPE) mainSliderTrack.style.transform = `translateX(${mainSliderPosition}px)`;
+  else reviewsSliderTrack.style.transform = `translateX(${reviewsSliderPosition}px)`;
 }
 
 const checkButtons = (a) => {
-  if (a == 1) {
-    btnPrev1.disabled = pos1 === 0; 
-    btnNext1.disabled = pos1 <= -itemWidth1(); 
+  if (a == MAIN_SLIDER_TYPE) {
+    mainSliderPreviousButton.disabled = mainSliderPosition === 0;
+    mainSliderNextButton.disabled = mainSliderPosition <= -countMainSliderWidth();
   } else {
-    btnPrev2.disabled = pos2 === 0; 
-    btnNext2.disabled = pos2 <= -3 * itemWidth2();
+    reviewsSliderPreviousButton.disabled = reviewsSliderPosition === 0;
+    reviewsSliderNextButton.disabled = reviewsSliderPosition <= -3 * countReviewsSliderWidth();
   }
 }
 
 document.addEventListener("DOMContentLoaded", () => {
-  checkButtons(1);
-  checkButtons(2);
+  checkButtons(MAIN_SLIDER_TYPE);
+  checkButtons(REVIEWS_SLIDER_TYPE);
 });
 
-const offset = document.documentElement.clientHeight / 2; // Расстояние скролла, при котором показать якорь
-const scrollUp = document.querySelector('.scroll-up'); // Собственно, якорь
+const scrollOffsetToShowScrollUp = document.documentElement.clientHeight / 2;
+const scrollUp = document.querySelector('.scroll-up');
 
-const getTop = () => window.pageYOffset || document.documentElement.scrollTop; // Считает позицию скролла по У
+const getTop = () => window.pageYOffset || document.documentElement.scrollTop; // scroll position (Y)
 
 window.addEventListener('scroll', () => { // Событие при скроллинге страницы
-  if (getTop() > offset) scrollUp.classList.add(act); // Если проскроллено больше расстояния скролла - якорь акт
-  else scrollUp.classList.remove(act); // Если нет - нет
+  if (getTop() > scrollOffsetToShowScrollUp) scrollUp.classList.add(activeAttribute); // Если проскроллено больше расстояния скролла - якорь акт
+  else scrollUp.classList.remove(activeAttribute); // Если нет - нет
 });
 
-scrollUp.addEventListener('click', () => { 
-  window.scrollTo({ 
+scrollUp.addEventListener('click', () => {
+  window.scrollTo({
     top: 0,
     behavior: 'smooth'
   });
 
-  if (scrollUp.classList.contains(act)) scrollUp.classList.remove(act);
+  if (scrollUp.classList.contains(activeAttribute)) scrollUp.classList.remove(activeAttribute);
   scrollUp.click();
 });
 
@@ -74,15 +90,15 @@ function validateNumbersInput(evt) {
   var theEvent = evt || window.event;
 
   if (theEvent.type === 'paste') {
-      key = event.clipboardData.getData('text/plain');
+    key = clipboardData.getData('text/plain');
   } else {
-      var key = theEvent.keyCode || theEvent.which;
-      key = String.fromCharCode(key);
+    var key = theEvent.keyCode || theEvent.which;
+    key = String.fromCharCode(key);
   }
   var regex = /[0-9]|\./;
-  if( !regex.test(key) ) {
+  if (!regex.test(key)) {
     theEvent.returnValue = false;
-    if(theEvent.preventDefault) theEvent.preventDefault();
+    if (theEvent.preventDefault) theEvent.preventDefault();
   }
 }
 
